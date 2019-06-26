ec7a2298f4f263e5e3054b4ddf6394f9a7d2ac07
Corv1n <nedsky123@gmail.com>
2019-06-26 23:54:51 +0300

    lessons.php

--- lessons.php
+++ lessons.php
@@ -1,3 +1,15 @@
 <?php
 
-test
+function minusPower($a, $b)
+{
+    $power = 1;
+
+    for($findPower = 1; $findPower <= -$b; $findPower++)
+    {
+        $power = 1 / ($a);
+    }
+
+    return $power;
+}
+
+echo minusPower(10, -1);
